c7780e93461243b3d0110a02d88e140b5fb3e635
Leonard Garvey <lengarvey@gmail.com>
2013-03-14 18:23:37 +1100

    some typography stuff, starting to build out homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
 describe 'the homepage', :type => :feature do
-  it 'should load on a simple get request' do
+  before :each do
     visit '/'
-    page.should have_content('Today I Ate:')
   end
+
+  it 'should load on a simple get request' do
+    page.should have_content('Today I ate')
+  end
+
+  it 'should let allow a user to enter a food log' do
+    fill_in 'Log', :with => 'A cheese burger, fries and a 200ml coke'
+    click_button 'Submit'
+    page.status_code.should == 200
+  end
+
 end
